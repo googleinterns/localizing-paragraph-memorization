1fb52799dbcc6d1c75935faceab4378db70ff50d
Mitchell Gordon <mitchellgordon@google.com>
2024-02-02 17:41:15 +0000

    Add readme, license, contributing, remove .ipynb_checkpoints/

--- .ipynb_checkpoints/setup-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-conda create -n venv
-conda activate venv
-pip install transformer_lens
-pip install pytest
-pip install ipykernel
-python3 -m ipykernel install --prefix "${DL_ANACONDA_HOME}" --name venv --display-name venv
